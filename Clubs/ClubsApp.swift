8c829c108244e3b34139fca44a4721927dd1ad76
Krasnokutskii Iaroslav <Yroslavkr1995@gmail.com>
2023-04-14 19:59:48 +0200

    Initial Commit

---
+++ Clubs/ClubsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubsApp.swift
+//  Clubs
+//
+//  Created by Yaroslav Krasnokutskiy on 14.4.23..
+//
+
+import SwiftUI
+
+@main
+struct ClubsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
